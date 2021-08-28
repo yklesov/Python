266394752e942b91775fd66f535e01ccf28951b5
Yura Klesov <58782605+yklesov@users.noreply.github.com>
2021-08-28 12:22:52 +0400

    Update ZadachiMOR.py

--- ZadachiMOR.py
+++ ZadachiMOR.py
@@ -39,3 +39,32 @@
 #         if s == s_reversed and c > maximum:
 #             maximum = int(s)
 # print (maximum)
+'''Какое самое маленькое число делится нацело на все числа от 1 до 20?'''
+# for i in range(2520,1000000):
+#     if i%2==0 and i%3==0 and i%5==0 and i%7==0 and i%11 and i%13==0 and i%17==0 and i%19==0:
+#         print(i)
+'''Найдите разность между суммой квадратов и квадратом суммы первых ста натуральных чисел.'''
+# c=0
+# z=0
+# for i in range(1,101):
+#     c+=i
+# c=c**2
+# for i in range(1,101):
+#     z+=i**2
+# print("Raznost = ",c-z)
+'''Какое число является 10001-м простым числом?'''
+# import math
+# count=0
+# for num in range(2,1000000000):
+#     if all(num%i!=0 for i in range(2,int(math.sqrt(num))+1)):
+#         count+=1
+#         if count==10001:
+#             print(num)
+#             break
+'''Существует только одна тройка Пифагора, для которой a + b + c = 1000.Найдите произведение abc.'''
+# for i in range(1,500):
+#     for j in range(1,500):
+#         for z in range(1,500):
+#             if i**2+j**2==z**2 and i+j+z==1000:
+#                 print(i,j,z)
+#                 print(i*j*z)
